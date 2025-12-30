65516b3ac759f24a3c6a717e35f8b81812983718
junnukim1007gmail.com <junnukim1007@gmail.com>
2025-12-30 14:56:44 +0900

    feat: 프로젝트 기본 설정

---
+++ README.md
@@ -0,0 +1,4 @@
+# Carelink
+![img_1.png](img_1.png)
+## Project Info
+서울시 병원 예약을 AI 챗봇을 이용하여 간편하게 진행해보세요
---
+++ src/main/java/com/normaldeve/carelinkbackend/CarelinkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.normaldeve.carelinkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarelinkBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarelinkBackendApplication.class, args);
+    }
+
+}
